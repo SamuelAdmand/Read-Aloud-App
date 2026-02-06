c91f972caed59685c0f2aefd79d3c4bc0a9fe975
SamuelAdmand <snowden06062013@protonmail.com>
2026-02-06 16:33:09 +0530

    Bump version to 1.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ val baseNamespace = "com.samuel.readaloud"
 val baseAppName = "Read Aloud"
 
 // --- Make changes to following code before commiting anything -----
-val myAppVersionCode = 2
-val appVersion = "1.0.0"
+val myAppVersionCode = 3
+val appVersion = "1.2.0"
 // --- End of the code ----
 
 android {
